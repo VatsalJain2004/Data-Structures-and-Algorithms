8aaffd7e7821951687d16bfaa3b65979c3f84e00
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2025-08-10 18:48:52 +0530

    Time: 0 ms (100%), Space: 7.9 MB (91.45%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -48,25 +48,84 @@
 
 
 
+// class Solution {
+// public:
+//     unordered_set<string> uset;
+//     void generatePowerOf2InSortedOrder() {
+//         for(int p = 0; p <= 29; p++) {  // 2 ^ x >= 1e9 ? x = 30, i.e at x = 29 2^x > 1e9.
+//             string temp = to_string(1 << p);
+//             sort(temp.begin(),temp.end());
+//             uset.insert(temp);
+//         }
+//         return;
+//     }
+
+//     bool reorderedPowerOf2(int n) {
+//         if(uset.empty()) {
+//             generatePowerOf2InSortedOrder();
+//         }
+
+//         string str = to_string(n);
+//         sort(str.begin(),str.end());
+//         return uset.count(str);
+//     }
+// };
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> getVectorCountFormat(int n) {
+//         vector<int> vec(10, 0);
+//         while(n) {
+//             vec[n%10]++;
+//             n = n / 10;
+//         }
+//         return vec;
+//     }
+
+//     bool reorderedPowerOf2(int n) {
+//         vector<int> vec = getVectorCountFormat(n);
+//         for(int i = 0; i < 30; i++) {
+//             if(vec == getVectorCountFormat(1 << i)) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
-    unordered_set<string> uset;
-    void generatePowerOf2InSortedOrder() {
-        for(int p = 0; p <= 29; p++) {  // 2 ^ x >= 1e9 ? x = 30, i.e at x = 29 2^x > 1e9.
-            string temp = to_string(1 << p);
-            sort(temp.begin(),temp.end());
-            uset.insert(temp);
+    int getIntergerCountFormat(int n) {
+        int num = 0;
+        while(n) {
+            num += pow(10, n%10);
+            n = n / 10;
         }
-        return;
+        return num;
     }
 
     bool reorderedPowerOf2(int n) {
-        if(uset.empty()) {
-            generatePowerOf2InSortedOrder();
+        int input = getIntergerCountFormat(n);
+        for(int i = 0; i < 30; i++) {
+            if(input == getIntergerCountFormat(1 << i)) {
+                return true;
+            }
         }
-
-        string str = to_string(n);
-        sort(str.begin(),str.end());
-        return uset.count(str);
+        return false;
     }
 };
