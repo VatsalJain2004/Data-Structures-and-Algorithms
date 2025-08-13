1b97c7b07655ddce29f32df17bff82fd4dd1b318
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2025-08-13 22:59:30 +0530

    Time: 10 ms (9.32%), Space: 8.9 MB (76.82%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,24 +1,50 @@
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//             long long ans = n;
+
+//         if(ans == 0)
+//             return false;
+        
+//         if(ans == 1)
+//             return true;
+        
+//         if(ans % 3 == 0){
+//             ans = ans / 3;
+//             return isPowerOfThree(ans);
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(!n) {
-            return false;
-        }
-
         int n_copy = n;
-        while(n_copy) {
-            if(n_copy == 1) {
-                return true;
-            }
-            if(!(n_copy % 3)) {
-                n_copy /= 3;
-            }
-            else {
-                return false;
-            }
-            cout << n_copy << endl;
+        while(n_copy % 3 == 0 && n_copy != 0) {
+            n_copy /= 3;
         }
-        return true;
-        // return n == 0 ? false : true;
+        return n_copy == 1;
     }
 };
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//         // If n is less than or equal to 0, it's not a power of three
+//         if (n <= 0) return false;
+        
+//         // 1162261467 is the largest power of 3 that fits within a 32-bit integer
+//         return 1162261467 % n == 0;
+//     }
+// };
