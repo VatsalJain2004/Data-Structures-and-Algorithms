98e4bb63876beee9c588b0fc030e50ab7675f90b
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2025-08-17 09:22:07 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.58%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -1,22 +1,64 @@
+// class Solution {
+// public:
+//     int maximum69Number (int num) {
+//         int index = -1;
+//         string numString = to_string(num);
+//         for(int i = 0; i < numString.length(); i++) {
+//             if(numString[i] == '6') {
+//                 numString[i] = '9';
+//                 index = i;
+//                 break;
+//             }
+//         }
+        
+//         cout << numString << endl;
+//         int num_copy = 0;
+//         for(int i = 0; i < numString.length(); i++) {
+//             num_copy = num_copy*10 + (numString[i]-'0');
+//         }   
+//         cout << num_copy << endl;
+//         return num_copy;
+//     }
+// };
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int maximum69Number (int num) {
+//         std::string s = std::to_string(num);
+//         for (char &c : s) {
+//             if (c == '6') { 
+//                 c = '9'; 
+//                 break; 
+//             }
+//         }
+//         return std::stoi(s);
+//     }
+// };
+
+
+
+
+
+
+
 class Solution {
 public:
     int maximum69Number (int num) {
-        int index = -1;
-        string numString = to_string(num);
-        for(int i = 0; i < numString.length(); i++) {
-            if(numString[i] == '6') {
-                numString[i] = '9';
-                index = i;
-                break;
+        int indexOf6 = -1, index = 0, num_copy = num;
+        while(num_copy) {
+            if(num_copy%10 == 6) {
+                indexOf6 = index;
             }
+            num_copy /= 10;
+            index++;
         }
-        
-        cout << numString << endl;
-        int num_copy = 0;
-        for(int i = 0; i < numString.length(); i++) {
-            num_copy = num_copy*10 + (numString[i]-'0');
-        }   
-        cout << num_copy << endl;
-        return num_copy;
+        return indexOf6 == -1 ? num : num + (3*pow(10, indexOf6));
     }
 };
