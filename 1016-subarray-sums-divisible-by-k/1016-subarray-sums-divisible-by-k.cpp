aab9a2d9a44dd2eca4ed37132cb5435244dc24a0
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2025-08-22 19:28:37 +0530

    Time: 0 ms (100%), Space: 34.3 MB (99.4%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -15,29 +15,46 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int subarraysDivByK(vector<int>& nums, int k) {
+//         unordered_map<int, int> umap;
+//         umap[0] = 1; // why ? take nothing from the array sum == 0 and 0 % k
+//         == 0, hence count + 1
 
+//         int n = nums.size(), count = 0;
+//         long long sum = 0;
+//         for(int num: nums) {
+//             sum += num;
+//             int rem = sum % k;
 
+//             if(rem < 0) {
+//                 rem += k;
+//             }
+
+//             if(umap.find(rem) != umap.end()) {
+//                 count += umap[rem];
+//             }
+//             umap[rem] += 1;
+//         }
+//         return count;
+//     }
+// };
 
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        unordered_map<int, int> umap;
-        umap[0] = 1; // why ? take nothing from the array sum == 0 and 0 % k == 0, hence count + 1
-
-        int n = nums.size(), count = 0;
-        long long sum = 0;
-        for(int num: nums) {
-            sum += num;
-            int rem = sum % k;
-            
-            if(rem < 0) {
-                rem += k;
-            }
-
-            if(umap.find(rem) != umap.end()) {
-                count += umap[rem];
-            }
-            umap[rem] += 1;
+        int n = nums.size();
+        vector<int> remfreq(k, 0);
+        remfreq[0] = 1;
+        
+        long long prefsum = 0;
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            prefsum += nums[i];
+            int r = ((prefsum % k) + k) % k;
+            count += remfreq[r];
+            remfreq[r]++;   
         }
         return count;
     }
