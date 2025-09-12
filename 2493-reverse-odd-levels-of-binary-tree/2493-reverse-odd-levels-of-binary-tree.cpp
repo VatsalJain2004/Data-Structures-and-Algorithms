8c91d0610fdb5fea4cfcecba2531d1860ba9279d
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2025-09-12 20:47:13 +0530

    Time: 0 ms (100%), Space: 77.2 MB (63.79%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -1,44 +1,73 @@
+// class Solution {
+// public:
+//     TreeNode* reverseOddLevels(TreeNode* root) {
+//         if (!root) {
+//             return nullptr;  
+//         }
+
+//         queue<TreeNode*> q;
+//         q.push(root);
+//         int level = 0;
+
+//         while (!q.empty()) {
+//             int size = q.size();
+//             vector<TreeNode*> currentLevelNodes;
+
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode* node = q.front();
+//                 q.pop();
+//                 currentLevelNodes.push_back(node);
+
+//                 if (node->left) q.push(node->left);
+//                 if (node->right) q.push(node->right);
+//             }
+
+//             if (level % 2 == 1) {
+//                 int left = 0, right = currentLevelNodes.size() - 1;
+//                 while (left < right) {
+//                     int temp = currentLevelNodes[left]->val;
+
+//                     currentLevelNodes[left]->val =
+//                         currentLevelNodes[right]->val;
+
+//                     currentLevelNodes[right]->val = temp;
+//                     left++;
+//                     right--;
+//                 }
+//             }
+
+//             level++;
+//         }
+
+//         return root; 
+//     }
+// };
+
+
+
+
+
+
 class Solution {
 public:
     TreeNode* reverseOddLevels(TreeNode* root) {
-        if (!root) {
-            return nullptr;  
+        traverseDFS(root->left, root->right, 0);
+        return root;
+    }
+
+private:
+    void traverseDFS(TreeNode* leftChild, TreeNode* rightChild, int level) {
+        if (leftChild == nullptr || rightChild == nullptr) {
+            return;
+        }
+  
+        if (level % 2 == 0) {
+            int temp = leftChild->val;
+            leftChild->val = rightChild->val;
+            rightChild->val = temp;
         }
 
-        queue<TreeNode*> q;
-        q.push(root);
-        int level = 0;
-
-        while (!q.empty()) {
-            int size = q.size();
-            vector<TreeNode*> currentLevelNodes;
-
-            for (int i = 0; i < size; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-                currentLevelNodes.push_back(node);
-
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-
-            if (level % 2 == 1) {
-                int left = 0, right = currentLevelNodes.size() - 1;
-                while (left < right) {
-                    int temp = currentLevelNodes[left]->val;
-
-                    currentLevelNodes[left]->val =
-                        currentLevelNodes[right]->val;
-
-                    currentLevelNodes[right]->val = temp;
-                    left++;
-                    right--;
-                }
-            }
-
-            level++;
-        }
-
-        return root; 
+        traverseDFS(leftChild->left, rightChild->right, level + 1);
+        traverseDFS(leftChild->right, rightChild->left, level + 1);
     }
 };
