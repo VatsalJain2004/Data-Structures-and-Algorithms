38be33f9e34124db89f21004d34ded5e3d7201c1
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2025-09-13 15:58:14 +0530

    Time: 5 ms (67.46%), Space: 19 MB (54.04%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -1,3 +1,23 @@
+// class Solution {
+// private:
+//     bool isVowel(char ch){
+//         return (ch == 'a' || ch == 'e' || ch == 'o' || ch == 'i' || ch == 'u');
+//     }
+    
+// public:
+//     bool doesAliceWin(string s) {
+//         int vowels = 0;
+//         for(auto ch : s) {
+//             if(isVowel(ch)) {
+//                 return true;
+//             }
+//         }
+        
+//         return false;
+//     }
+// };
+
+
 class Solution {
 private:
     bool isVowel(char ch){
@@ -9,7 +29,7 @@ public:
         int vowels = 0;
         for(auto ch : s) {
             if(isVowel(ch)) {
-                vowels++;
+                vowels += 1;
             }
         }
         
