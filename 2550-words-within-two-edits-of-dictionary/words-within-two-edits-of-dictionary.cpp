77d6e1ccc22a17cbd67985a38dd9c1f86404a512
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-04-23 05:13:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (71.57%) - LeetSync

--- 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
         vector<string> result;
+        int n = queries[0].length();
 
         for (auto &q : queries) {
             for (auto &d : dictionary) {
                 int diff = 0;
 
-                for (int i = 0; i < q.size(); i++) {
+                for (int i = 0; i < n; i++) {
                     if (q[i] != d[i]) {
                         diff++;
                         if (diff > 2) break;
