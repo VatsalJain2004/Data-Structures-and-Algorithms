ce0fbb6bdf0b2c5a4ff83e7d24ee28f8a72d9a86
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-08-04 03:48:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (74.46%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        return dfs(n, dp);
+        if (n <= 1) return n;
+        // memoization...
+        // dfs(n-1) --> prev, dfs(n-2) --> prev'prev
+        int prev1 = 1, prev2 = 1, count = prev1 + prev2;
+        for (int i = 2; i <= n; i++) {
+            count = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = count;
+        }
+
+        return prev1;
     }
 
+    // dfs solution
     int dfs(int n, vector<int>& dp) {
         if (n <= 1) return dp[n] = 1;
         if (dp[n] != -1) return dp[n];
