bf5efd4485e2aa978004af2767c6b3be32694e8e
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-08-05 18:27:08 +0530

    Time: 0 ms (100%), Space: 10.7 MB (47.35%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,47 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        int minus1 = nums[0], minus2 = 0;
-        for(int i = 1; i < n; i++) {
-            int p = nums[i];
-            if(i > 1) {
-                p += minus2;
-            }
-            int np = minus1;
-            int cur = max(p, np);
-            minus2 = minus1;
-            minus1 = cur;
+    int n;
+    vector<int> dp;
+    vector<int> nums;
+
+    int robOrLeaveMemoization (int i, int moneyRobbed) {
+        if (i < 0) return 0;
+        if (i == 0) return nums[0];
+        if (dp[i] != -1) return dp[i];
+
+        int rob = nums[i] + robOrLeaveMemoization (i-2, moneyRobbed + nums[i]);
+        int leave = robOrLeaveMemoization (i-1, moneyRobbed);
+
+        return dp[i] = max({rob, leave});
+    }
+
+    int robOrLeaveTabulation () {
+        dp[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            int rob = nums[i] + (i > 1 ? dp[i-2] : 0);
+            int leave = (i > 0) ? dp[i-1] : 0;
+            dp[i] = max({rob, leave});
         }
-        return minus1;
+        return dp[n-1];
+    }
+    
+    int robOrLeaveSpaceOptimization () {
+        dp.clear();
+        int prevRobbery = max(nums[0], n > 1 ? nums[1] : -1), secondPrevRobbery = nums[0];
+        for (int i = 2; i < n; i++) {
+            int rob = nums[i] + secondPrevRobbery, 
+                leave = prevRobbery;
+
+            secondPrevRobbery = prevRobbery;
+            prevRobbery = max(rob, leave);
+        }
+        return prevRobbery;
+    }
+
+    int rob(vector<int>& nums) {
+        n = nums.size();
+        dp = vector<int>(n, -1);
+        this -> nums = nums;
+        return robOrLeaveSpaceOptimization();    
     }
 };
