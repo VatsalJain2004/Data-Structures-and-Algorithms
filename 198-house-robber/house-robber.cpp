2028adfd7576dea0be2161ff6dc01995a29cd4b2
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-08-05 18:03:57 +0530

    Time: 3 ms (6.08%) | Memory: 10.7 MB (24.31%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,19 +2,31 @@ class Solution {
 public:
     int n;
     vector<int> dp;
-    int robOrLeave (int i, int moneyRobbed, vector<int>& nums) {
+    vector<int> nums;
+    int robOrLeaveMemoization (int i, int moneyRobbed) {
         if (i < 0) return 0;
         if (dp[i] != -1) return dp[i];
 
-        int rob = nums[i] + robOrLeave (i-2, moneyRobbed + nums[i], nums);
-        int leave = robOrLeave (i-1, moneyRobbed, nums);
+        int rob = nums[i] + robOrLeaveMemoization (i-2, moneyRobbed + nums[i]);
+        int leave = robOrLeaveMemoization (i-1, moneyRobbed);
 
         return dp[i] = max({rob, leave});
     }
 
+    int robOrLeaveTabulation () {
+        dp[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            int rob = nums[i] + (i > 1 ? dp[i-2] : 0);
+            int leave = (i > 0) ? dp[i-1] : 0;
+            dp[i] = max({rob, leave});
+        }
+        return dp[n-1];
+    }
+    
     int rob(vector<int>& nums) {
         n = nums.size();
         dp = vector<int>(n, -1);
-        return robOrLeave(n-1, 0, nums);    
+        this -> nums = nums;
+        return robOrLeaveTabulation();    
     }
 };
