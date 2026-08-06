ccb163522ce37c5c5aab55a6701b597f33ed27be
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-08-06 15:31:22 +0530

    Time: 59 ms (14.45%) | Memory: 49 MB (37.71%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -4,7 +4,29 @@ public:
     vector<vector<int>> dp;
     int n;
 
-    int coinChangeRecursion (int i, int target) {
+    int coinChangeTabulation (int& amount) {
+        for(int i = 0; i <= amount; i++){
+            if(i % coins[0] == 0)
+               dp[0][i] = i / coins[0];
+            else
+                dp[0][i] = 1e9;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                int pick = 1e9;
+                if (j >= coins[i]) 
+                    pick = 1 + dp[i][j - coins[i]];
+
+                int npick = dp[i-1][j];
+                dp[i][j] = min(pick, npick);
+            }
+        }
+
+        return dp[n-1][amount];
+    }
+
+    int coinChangeMemoization (int i, int target) {
         if (i == 0) {
             if (target % coins[i] == 0) return dp[i][target] = (target / coins[i]);
             else return dp[i][target] = 1e9;
@@ -14,8 +36,8 @@ public:
 
         int pick = 1e9;
         if (target >= coins[i])
-            pick = 1 + coinChangeRecursion (i, target-coins[i]);
-        int npick = coinChangeRecursion (i-1, target);
+            pick = 1 + coinChangeMemoization (i, target-coins[i]);
+        int npick = coinChangeMemoization (i-1, target);
 
         return dp[i][target] = min(pick, npick);
     }
@@ -25,9 +47,9 @@ public:
 
         this -> coins = coins;
         n = coins.size();
-        dp = vector<vector<int>>(n, vector<int>(amount+1, -1));
-        int coinsNeeded = coinChangeRecursion (n-1, amount);
 
-        return (coinsNeeded == 1e9 || coinsNeeded == 0) ? -1 : coinsNeeded;
+        // size --> rows, amount --> columns
+        dp = vector<vector<int>>(n, vector<int>(amount+1, 0));
+        return coinChangeTabulation (amount) == 1e9 ? -1 : dp[n-1][amount];
     }
 };
