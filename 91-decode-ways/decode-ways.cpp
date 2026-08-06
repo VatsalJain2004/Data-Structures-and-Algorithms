1fc717878c92c9253ff088734b6a98aa66739e47
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-08-06 11:45:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (46.22%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -43,6 +43,28 @@ public:
         return dp[i] = pick1 + pick2;
     }
 
+    int numDecodingsTabulation() {
+        dp[n] = 1;
+
+        for (int i = n-1; i >= 0; i--) {
+            if (s[i] == '0') {
+                dp[i] = 0;
+            }
+            else {
+                int pick1 = dp[i+1];
+                int pick2 = 0;
+                if ((i + 1 < n) && 
+                        ((s[i] == '1') || (s[i] == '2' && s[i + 1] <= '6'))
+                    )
+                    pick2 = dp[i+2];
+
+                dp[i] = pick1 + pick2;
+            }
+        }
+
+        return dp[0];
+    }
+
     int numDecodings(string s) {
         n = s.length();
         this->s = s;
