1f4d78e0716478b5d23f2dfe366aefbf23b998bf
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-08-08 13:05:11 +0530

    Time: 205 ms (9.79%), Space: 17.6 MB (54.74%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int n;
     vector<int> nums;
-    vector<vector<int>> dp;
+    vector<vector<bool>> dp;
 
     bool canPartitionRecursion (int n, int target) {
         if (n < 0) return false;
-        if (target == 0) return true;
+        if (target == 0) return dp[n][target] = true;
 
-        if (dp[n][target] != -1) return dp[n][target];
+        if (dp[n][target] != false) return dp[n][target];
 
         bool take = false;
         if (target >= nums[n])
@@ -19,16 +19,37 @@ public:
         return dp[n][target] = (take || notTake);
     }
 
+    bool canPartitionTabulation(int target) {
+        for (int i = 0; i <= n; i++) dp[i][0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= target; j++) {
+                if (dp[i][j]) continue;
+
+                bool notTake = dp[i-1][j];
+                bool take = false;
+                if (j >= nums[i-1])
+                    take = dp[i-1][j-nums[i-1]];
+
+                dp[i][j] = take || notTake;
+            }
+        }
+
+        return dp[n][target];
+    }
+
     bool canPartition(vector<int>& nums) {
         n = nums.size();
         this -> nums = nums;
      
-        int sum = 0;
+        int sum = 0, target = 0;
         for (int& it: nums) sum += it;
         if (sum % 2) return false;
 
-        this -> dp = vector<vector<int>>(n+1, vector<int>((sum/2) + 1, -1));
+        target = sum / 2;
 
-        return canPartitionRecursion (n-1, sum / 2);
+        this -> dp = vector<vector<bool>>(n+1, vector<bool>(target+1, false));
+
+        return canPartitionTabulation (target);
     }
 };
