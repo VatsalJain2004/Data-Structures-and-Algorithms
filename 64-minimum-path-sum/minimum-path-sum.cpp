b13a52e27acda0640c6f3860419f8c7e9327fd4a
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-08-09 12:50:44 +0530

    Time: 3 ms (30.35%) | Memory: 17.3 MB (16.98%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -19,10 +19,30 @@ public:
         return dp[r][c] = grid[r][c] + min (memoization(r-1, c), memoization(r, c-1));
     }
 
+    int tabulation () {
+        dp = vector<vector<int>>(m, vector<int>(n, 0));
+
+        dp[0][0] = grid[0][0];
+        for (int c = 1; c < n; c++) {
+            dp[0][c] = grid[0][c] + dp[0][c - 1];
+        }
+        
+        for (int r = 1; r < m; r++) {
+            dp[r][0] = grid[r][0] + dp[r - 1][0];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = grid[i][j] + min (dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
     int minPathSum(vector<vector<int>>& grid) {
         m = grid.size(), n = grid[0].size();
         this -> grid = grid;
         dp = vector<vector<int>>(m, vector<int>(n, -1));
-        return memoization (m-1, n-1);
+        return tabulation ();
     }
 };
