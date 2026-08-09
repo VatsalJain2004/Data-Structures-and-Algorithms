65ec484e5e4737dc7f2c3a6eb842c5b6a3ba7f2f
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-08-09 11:41:27 +0530

    Time: 0 ms (100%), Space: 9.4 MB (31.29%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    // int count = 0;
-    // vector<vector<int>> dp;
+    vector<vector<int>> dp;
 
-    int uniquePaths(int m, int n) {   
+    int uniquePaths(int m, int n) {  
+        dp = vector<vector<int>>(m+1, vector<int>(n+1, -1)); 
         // Not Needed for explorePathsSpaceOptimization => dp = vector<vector<int>>(m, vector<int>(n, -1));
         return explorePathsSpaceOptimization (m, n);
     }
@@ -24,29 +24,28 @@ public:
         return prevDP[n-1];
     } 
     
-    // int explorePathsTabular (int m, int n) {
-    //     vector<vector<int>> dp(m, vector<int>(n, 0));
-    //     dp[0][0] = 1;
-    //     for (int i = 0; i < m; i++) {
-    //         for (int j = 0; j < n; j++) {
-    //             if (i == 0 && j == 0) dp[i][j] = 1;
-    //             else {
-    //                 dp[i][j] = 0;
-    //                 if (i > 0) dp[i][j] += dp[i-1][j];
-    //                 if (j > 0) dp[i][j] += dp[i][j-1];
-    //             }
-    //         }
-    //     }
-    //     return dp[m-1][n-1];
-    // } 
+    int explorePathsTabular (int m, int n) {
+        dp = vector<vector<int>>(m, vector<int>(n, 0));
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) dp[i][j] = 1;
+                else {
+                    dp[i][j] = 0;
+                    if (i > 0) dp[i][j] += dp[i-1][j];
+                    if (j > 0) dp[i][j] += dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    } 
     
+    int explorePathsMemoization (int m, int n) {
+        if (m < 0 || n < 0) return 0;
+        if (m == 0 && n == 0) return 1;
 
-    // int explorePathsMemoization (int m, int n) {
-    //     if (m < 0 || n < 0) return 0;
-    //     if (m == 0 && n == 0) return 1;
+        if (dp[m][n] != -1) return dp[m][n];
 
-    //     if (dp[m][n] != -1) return dp[m][n];
-
-    //     return dp[m][n] = explorePathsMemoization (m-1, n) + explorePathsMemoization (m, n-1);
-    // }
+        return dp[m][n] = explorePathsMemoization (m-1, n) + explorePathsMemoization (m, n-1);
+    }
 };
