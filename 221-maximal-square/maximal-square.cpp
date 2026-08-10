03b7e9ebe47168340e2df9c06e1630880d4fa464
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-08-10 10:47:41 +0530

    Time: 11 ms (49.06%) | Memory: 35.4 MB (5.26%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -4,29 +4,52 @@ public:
     vector<vector<char>> grid;
     vector<vector<int>> dp;
 
-    int recursive (int r, int c) {
-        if (r >= n || c >= m) return 0;
-        if (dp[r][c] != -1) return dp[r][c];
+    int memoization(int r, int c) {
+        if (r >= n || c >= m)
+            return 0;
+        if (dp[r][c] != -1)
+            return dp[r][c];
 
-        int right = recursive (r, c+1);
-        int diagonal = recursive (r+1, c+1);
-        int down = recursive (r+1, c);
+        int right = memoization(r, c + 1);
+        int diagonal = memoization(r + 1, c + 1);
+        int down = memoization(r + 1, c);
 
-        if (grid[r][c] == '0') return dp[r][c] = 0;
+        if (grid[r][c] == '0')
+            return dp[r][c] = 0;
 
         int squareSize = 1 + min({right, down, diagonal});
         maxSquareSize = max(maxSquareSize, squareSize);
 
         return dp[r][c] = squareSize;
     }
- 
+
+    int tabulation() {
+        dp = vector<vector<int>>(n, vector<int>(m, 0));
+
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
+                if (grid[r][c] == '1') {
+                    int right = (c - 1 >= 0) ? dp[r][c - 1] : 0;
+                    int down = (r - 1 >= 0) ? dp[r - 1][c] : 0;
+                    int diagonal =
+                        (r - 1 >= 0 && c - 1 >= 0) ? dp[r - 1][c - 1] : 0;
+
+                    dp[r][c] = 1 + min({right, down, diagonal});
+
+                    maxSquareSize = max(maxSquareSize, dp[r][c]);
+                }
+            }
+        }
+
+        return maxSquareSize * maxSquareSize;
+    }
+
     int maximalSquare(vector<vector<char>>& matrix) {
         n = matrix.size(), m = matrix[0].size();
         maxSquareSize = 0;
         grid = matrix;
-        dp = vector<vector<int>> (n, vector<int>(m, -1));
-        
-        recursive (0, 0);
-        return maxSquareSize * maxSquareSize;
+        dp = vector<vector<int>>(n, vector<int>(m, -1));
+
+        return tabulation();
     }
 };
