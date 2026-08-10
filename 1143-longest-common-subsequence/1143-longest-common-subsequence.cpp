ed785b8d58d74d1692e7068d75211f73f757ca53
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-08-10 14:09:51 +0530

    Time: 35 ms (34.67%), Space: 27.5 MB (55.65%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int n, m;
+    string s, t;
+
+    vector<vector<int>> dp;
+
+    int recursive (int i, int j) {
+        if (i < 0 || j < 0) return 0;
+        if (s[i] == t[j]) return 1 + recursive (i-1, j-1);
+
+        return max(recursive (i-1, j), recursive (i, j-1));
+    }
+
+    int memoization (int i, int j) {
+        if (i < 0 || j < 0) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        if (s[i] == t[j]) return dp[i][j] =  1 + memoization (i-1, j-1);
+
+        return dp[i][j] = max(memoization (i-1, j), memoization (i, j-1));
+    }
+
+    int tabulation () { 
+        dp = vector<vector<int>>(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (s[i] == s[j]) {
+                    dp[i][j] = 1 + ((i > 0 && j > 0) ? dp[i-1][j-1] : 0);
+                }
+                else {
+                    dp[i][j] = max((i > 0 ? dp[i-1][j] : 0), (j > 0 ? dp[i][j-1] : 0));
+                }
+            }
+        }
+
+        return dp[n-1][m-1];
+    }
+
+    int longestCommonSubsequence(string s, string t) {
+        n = s.length(), m = t.length();    
+        this -> s = s;
+        this -> t = t;
+
+        dp = vector<vector<int>>(n, vector<int>(m, -1));
+
+        return memoization (n-1, m-1);
+    }
+};
