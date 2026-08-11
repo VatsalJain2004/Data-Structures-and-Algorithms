e3c7b2bcf2ef7c40ec1f2d3d2c8a1621b8ba7bde
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-08-11 17:24:43 +0530

    Time: 31 ms (53.26%) | Memory: 44.3 MB (32.58%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int n, m;
+    string s, t;
+    vector<vector<int>> dp;
+
+    int recursive (int i, int j) {
+        if (j < 0) return 1;
+        if (i < 0) return 0;
+
+        if (s[i] == t[j])
+            return recursive (i-1, j-1) + recursive (i-1, j);
+        
+        return recursive (i-1, j);
+    }
+    
+    int memoization (int i, int j) {
+        if (j < 0) return 1;
+        if (i < 0) return 0;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s[i] == t[j]) return dp[i][j] = (memoization (i-1, j-1) + memoization (i-1, j));
+        
+        return dp[i][j] = memoization (i-1, j);
+    }
+
+    int tabulation () {
+        vector<vector<double>> ldp(n+1, vector<double>(m+1, 0));
+
+        for (int i = 0; i <= n; i++) ldp[i][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i-1] == t[j-1]) {
+                    ldp[i][j] = ldp[i-1][j-1] + ldp[i-1][j];
+                }
+                else {
+                    ldp[i][j] = ldp[i-1][j];
+                }
+            }
+        }
+
+        return (int)ldp[n][m];
+    }
+
+    int numDistinct(string s, string t) {
+        this -> s = s;
+        this -> t = t;
+        n = (this -> s).length(), m = (this -> t).length();
+
+        // if (m > n || m == 0 || n == 0) return 0;
+
+        return tabulation ();
+    }
+};
