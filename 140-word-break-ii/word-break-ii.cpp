aafd4abcb13053fd610ae7f3c96647f2cd6d103e
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-08-12 11:29:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (69.20%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int n;
+
+    string s;
+    vector<string> ans;
+    unordered_set<string> dictSet;
+
+    void recursive (int i, string sentence) {
+        if (i > n) return;
+        if (i == n) {
+            sentence.pop_back();
+            ans.push_back(sentence);
+            return;
+        }
+
+        string word = "";
+        for (int j = i; j < n; j++) {
+            word += s[j];
+            sentence += s[j];
+            if (dictSet.count(word)) {
+                sentence += ' ';
+                recursive (j+1, sentence);
+                sentence.pop_back();
+            }
+        }
+    }
+
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        this -> s = s;
+        dictSet = unordered_set<string>(wordDict.begin(), wordDict.end());
+        n = (this -> s).length();
+
+        recursive(0, "");
+
+        return ans;
+    }
+};
