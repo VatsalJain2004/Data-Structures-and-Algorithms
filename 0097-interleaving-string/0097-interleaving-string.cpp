08c61540170233c096d33e61b95ca0bb730becb7
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-08-13 11:24:23 +0530

    Time: 37 ms (7.62%), Space: 17.5 MB (15.93%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -2,20 +2,35 @@ class Solution {
 public:
     int n, m, l;
     string s, t, r;
-    int dp[101][101][201];
+    int dp3[101][101][202];
+    int dp2[101][101];
 
-    bool recursive (int i, int j, int k) {
+    bool recursive3D (int i, int j, int k) {
         if (k == l && i == n && j == m) return true; 
         if (k >= l) return false;
 
-        if (dp[i][j][k] != -1) return dp[i][j][k];
+        if (dp3[i][j][k] != -1) return dp3[i][j][k];
 
         bool result = false;
-        if (s[i] == r[k]) result = recursive (i+1, j, k+1); 
-        if (result) return dp[i][j][k] = result; 
+        if (s[i] == r[k]) result = recursive3D (i+1, j, k+1); 
+        if (result) return dp3[i][j][k] = result; 
 
-        if (t[j] == r[k]) result = recursive (i, j+1, k+1);
-        return dp[i][j][k] = result; 
+        if (t[j] == r[k]) result = recursive3D (i, j+1, k+1);
+        return dp3[i][j][k] = result; 
+    }
+    
+    bool recursive2D (int i, int j) {
+        if (i+j == l && i == n && j == m) return true; 
+        if (i+j >= l) return false;
+
+        if (dp2[i][j] != -1) return dp2[i][j];
+
+        bool result = false;
+        if (s[i] == r[i+j]) result = recursive2D (i+1, j); 
+        if (result) return dp2[i][j] = result; 
+
+        if (t[j] == r[i+j]) result = recursive2D (i, j+1);
+        return dp2[i][j] = result; 
     }
 
     bool isInterleave(string s1, string s2, string s3) {
@@ -26,7 +41,8 @@ public:
         n = s.length(), m = t.length(), l = r.length();
         if (n+m < l) return false; 
 
-        memset(dp, -1, sizeof(dp));
-        return recursive (0, 0, 0);
+        memset(dp2, -1, sizeof(dp2));
+        memset(dp3, -1, sizeof(dp3));
+        return recursive2D (0, 0);
     }
 };
