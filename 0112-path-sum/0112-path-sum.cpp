29ee81e1b87b656b6769b87b26bab1fe3573d500
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-08-13 19:01:57 +0530

    Time: 0 ms (100%), Space: 22 MB (36.39%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -13,9 +13,47 @@ class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if (!root) return 0;
+        return (root -> val) % 2 ? dfs (root, targetSum) : bfs (root, targetSum);
+    }
+
+    bool dfs (TreeNode* root, int targetSum) {
+        if (!root) return 0;
         if (!root -> left && !root -> right) return root -> val == targetSum;
 
         targetSum -= root -> val;
         return hasPathSum (root -> left, targetSum) || hasPathSum (root -> right, targetSum);
+    }
+
+    bool bfs (TreeNode* root, int& targetSum) {
+        if (!root) return false;
+
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, targetSum - root->val});
+
+        while (!q.empty()) {
+            auto [node, remaining] = q.front();
+            q.pop();
+
+            if (!node->left && !node->right &&
+                remaining == 0) {
+                return true;
+            }
+
+            if (node->left) {
+                q.push({
+                    node->left,
+                    remaining - node->left->val
+                });
+            }
+
+            if (node->right) {
+                q.push({
+                    node->right,
+                    remaining - node->right->val
+                });
+            }
+        }
+
+        return false;
     }
 };
