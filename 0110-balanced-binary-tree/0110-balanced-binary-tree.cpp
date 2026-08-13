c0202c1d208d884938bc53004389e9bbd12f4c7a
Vatsal Jain <107668632+VatsalJain2004@users.noreply.github.com>
2026-08-13 18:20:18 +0530

    Time: 0 ms (100%), Space: 23.2 MB (21.07%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -15,16 +15,28 @@ public:
     bool heightBalance = true;
 
     bool isBalanced(TreeNode* root) {
-        diameterOfBinaryTree(root);      
-        return heightBalance;
+        return function2(root) != -1;
+    }
+    
+    int function2 (TreeNode* root) {
+        if (!root) return 0;
+
+        int left = function2 (root -> left);
+        if (left == -1) return -1;
+        int right = function2 (root -> right);
+        if (right == -1) return -1;
+        
+        if (abs(left - right) > 1) return -1;
+
+        return 1 + max(left, right);
     }
 
-    int diameterOfBinaryTree(TreeNode* root) {
+    int function1(TreeNode* root) {
         if (!root) return 0;
         if (!root -> left && !root -> right) return 1;
 
-        int left = diameterOfBinaryTree (root -> left);
-        int right = diameterOfBinaryTree (root -> right);
+        int left = function1 (root -> left);
+        int right = function1 (root -> right);
         
         if (abs(left - right) > 1) heightBalance = false;
 
